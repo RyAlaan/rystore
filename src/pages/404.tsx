59e96266bb5d80b375ed2f47b5ea49449550b1f2
RyAlaan <alantisderry@gmail.com>
2023-10-02 23:18:15 -0700

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,9 +2,13 @@ import Link from "next/link"
 
 const NotFoundPage = () => {
     return (
-        <>
-            <h1>Salah Link jir woilah</h1>
-        </>
+        <div className="flex flex-col justify-center w-full h-screen align-center">
+            <div className="flex flex-col gap-10 align-center py-10">
+                <h1 className="text-8xl text-center font-medium">404 Not Found</h1>
+                <p className="text-center">Your visited page not found. You may go home page.</p>
+            </div>
+            <Link href={"/"} className="px-12 py-4 text-white bg-secondary w-fit self-center">Back to homepage</Link>
+        </div>
     )
 }
 
