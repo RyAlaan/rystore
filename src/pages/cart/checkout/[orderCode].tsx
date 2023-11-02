d3e139442e754bab7a4243ec50be13123316ec12
RyAlaan <alantisderry@gmail.co>
2023-11-02 19:08:41 +0700

    commit checkout

--- src/pages/cart/checkout/[orderCode].tsx
+++ src/pages/cart/checkout/[orderCode].tsx
@@ -1,3 +1,4 @@
+import Input from "@/components/elements/Input";
 import { useRouter } from "next/router";
 
 const checkoutPage = () => {
@@ -7,7 +8,13 @@ const checkoutPage = () => {
   
 
   return (
-    <div className="px-2 lg:px-8 flex flex-col justify-between gap-y-3"></div>
+    <form className="px-2 lg:px-8 flex flex-col justify-between gap-y-3">
+      <div className="">
+        <h1>Billing Details</h1>
+        <Input type="text" placeholder="Full Name" name="fullname" className="bg-[#F5F5F5]"></Input>
+      </div>
+      <div className=""></div>
+    </form>
   );
 };
 
