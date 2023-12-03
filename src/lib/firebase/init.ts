e7299a429420ca9b89359f41803dc9ec0529cef2
RyAlaan <alantisderry@gmail.co>
2023-12-03 15:58:51 +0700

    db connect

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -3,12 +3,12 @@ import { Firestore, getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyC2dwJv82pIqdjq7Mez5Yn8r_-9uxNnEuU",
+  authDomain: "rystore-eae2d.firebaseapp.com",
+  projectId: "rystore-eae2d",
+  storageBucket: "rystore-eae2d.appspot.com",
+  messagingSenderId: "610908197214",
+  appId: "1:610908197214:web:5842c344bc102a2f153fcf",
 };
 
 // Inisialisasi Firebase
